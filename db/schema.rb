4f5d2b74da119953c6459e48f9a49c69ca7aa203
mindytai175 <mindytai175@gmail.com>
2019-01-22 11:31:57 +0800

    rename users column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_21_160149) do
+ActiveRecord::Schema.define(version: 2019_01_22_032320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2019_01_21_160149) do
   create_table "users", force: :cascade do |t|
     t.string "user_name"
     t.string "password"
-    t.boolean "is_admin"
+    t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
